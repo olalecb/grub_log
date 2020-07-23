f3eaa92671448e81bfcdd3400cfd9e1880fc4422
olalecb <alec.brown@oracle.com>
2020-07-23 17:14:01 -0400

    sl-stat won't print kmsg without printing log

--- sl-stat.c
+++ sl-stat.c
@@ -1,3 +1,41 @@
+/*
+ * sl-stat: Linux app that can display register, heap, bootloader log,
+ *          snd slaunch information.
+ *
+ * Copyright (c) 2020, Oracle and/or its affiliates.
+ *
+ * Copyright (c) 2006-2011, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -25,10 +63,8 @@ static void read_log_file(char *filename)
 
 	log_fd = open(filename, O_RDONLY);
 
-	if (errno != 0) {
-		fprintf(stderr, "Error opening %s: %s\n", filename, strerror(errno));
-		exit(EXIT_FAILURE);
-	}
+	if (errno != 0)
+		return;
 
 	file_size = lseek(log_fd, 0, SEEK_END);
 	lseek(log_fd, 0, SEEK_SET);
@@ -40,10 +76,10 @@ static void read_log_file(char *filename)
 	do {
 		log_offset = (bootloader_log_t *)((uint8_t *)log + offset);
 		bytes_read = read(log_fd, log_offset, file_size);
-		printf("bytes_read = %d\n", bytes_read);
+
 		if (errno != 0) {
 			fprintf(stderr, "Error reading %s: %s\n", filename, strerror(errno));
-			exit(EXIT_FAILURE);
+			break;
 		}
 
 		offset += bytes_read;
@@ -53,7 +89,7 @@ static void read_log_file(char *filename)
 	
 	if (errno != 0) {
 		fprintf(stderr, "Error closing %s: %s\n", filename, strerror(errno));
-		exit(EXIT_FAILURE);
+		return;
 	}
 }
 
@@ -68,7 +104,7 @@ static void print_bootloader_log(void)
 		return;
 
 	offset = sizeof(*log);
-
+	
 	while (offset < log->next_off) {
 		msgs = (bootloader_log_msg_t *) ((uint8_t *) log + offset);
 
@@ -817,7 +853,6 @@ static void print_kmsg(void)
 
 	do {
 		read_size = read(kmsg_fd, buf, BUFSIZ);
-
 		if (read_size > 0) {
 			msg = parse_kmsg(buf, tv);
 			if (is_slaunch(msg))
